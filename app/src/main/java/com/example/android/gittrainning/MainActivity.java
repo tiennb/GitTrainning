d8222cea663dc5852eea1b05f6b04cf87a4002d0
android1_hiep <hiepnd230992@gmail.com>
2015-08-13 16:58:24 +0700

    add string

--- app/src/main/java/com/example/android/gittrainning/MainActivity.java
+++ app/src/main/java/com/example/android/gittrainning/MainActivity.java
@@ -13,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
+        System.out.println("Tiến t hó");
         initView();
     }
 
