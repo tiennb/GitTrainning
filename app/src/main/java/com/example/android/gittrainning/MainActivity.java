1d1f383a26b2f4a7b3aab9d1398fa77a9cab8ffc
Tiennb GVS <tiennb.gemtek@gmail.com>
2015-08-13 14:44:03 +0700

    test 1

--- app/src/main/java/com/example/android/gittrainning/MainActivity.java
+++ app/src/main/java/com/example/android/gittrainning/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Change 1
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
