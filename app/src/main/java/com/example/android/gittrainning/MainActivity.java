e78e995d42559c465ee9e0a8f557c832cd434780
Tiennb GVS <tiennb.gemtek@gmail.com>
2015-08-14 14:26:21 +0700

    test 2

--- app/src/main/java/com/example/android/gittrainning/MainActivity.java
+++ app/src/main/java/com/example/android/gittrainning/MainActivity.java
@@ -13,13 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Tiến t hó 1");
-        System.out.println("Tiến t hóTiến t hó 1");
-        System.out.println("Tiến t hóTiến t hó 1"); 
-
-
-
-
         System.out.println("Tiến t hó");
         System.out.println("Tiến t hó");
         System.out.println("Tiến t hó");
